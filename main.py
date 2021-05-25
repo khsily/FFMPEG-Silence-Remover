8a8e4e7ba49f1c1f7c6b9833f8f9ed7d8c431525
khsily <khsilyqwerty@gmail.com>
2021-05-25 12:21:18 +0900

    Add file filter on selecting files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import filedialog
 from unsilence import unsilence
 
-inputs = filedialog.askopenfilenames(title="변환할 음원 선택")
+inputs = filedialog.askopenfilenames(title="변환할 음원 선택", filetypes=[("Audio Files", ".mp3 .wav .ogg")])
 outPath = filedialog.askdirectory(title="저장 경로 선택")
 
 for idx, input in enumerate(inputs):
