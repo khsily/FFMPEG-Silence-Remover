c40567c072c6555b789c937ac9d580bb5f8a2290
khsily <khsilyqwerty@gmail.com>
2021-05-25 20:20:47 +0900

    Remove TK dialog

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from tkinter import *
 from tkinter import filedialog
 from unsilence import unsilence
 
-inputs = filedialog.askopenfilenames(title="변환할 음원 선택", filetypes=[("Audio Files", ".mp3 .wav .ogg")])
-outPath = filedialog.askdirectory(title="저장 경로 선택")
+def main():
+  Tk().withdraw()
+  inputs = filedialog.askopenfilenames(title="변환할 음원 선택", filetypes=[("Audio Files", ".mp3 .wav .ogg")])
+  outPath = filedialog.askdirectory(title="저장 경로 선택")
 
-for idx, input in enumerate(inputs):
-  unsilence(input, outPath)
+  if (not inputs or not outPath):
+    return
+
+  for input in inputs:
+    unsilence(input, outPath)
+
+
+main()
