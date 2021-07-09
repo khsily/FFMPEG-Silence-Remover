a84cb5170b3ebc57f94b0cc09b6d9529ae158a9b
khsily <khsilyqwerty@gmail.com>
2021-07-09 12:13:33 +0900

    Fix crash on korean filename error

--- unsilence.py
+++ unsilence.py
@@ -4,7 +4,7 @@ this_script_dir = os.path.dirname(os.path.realpath(__file__))
 ffmpeg_path = this_script_dir + '\\ffmpeg.exe'
 
 def unsilence(input, outPath):
-  filename = input.split('/')[-1]
+  filename = os.path.basename(input)
   name = filename.split('.')[0]
   ext = filename.split('.')[-1]
   outputName = outPath + '/' + name
@@ -12,13 +12,14 @@ def unsilence(input, outPath):
   outS = outputName + 'S' + '.wav'
 
   commands = [
+    'rmdir /s /q .ffmpeg-tmp',
     f'mkdir .ffmpeg-tmp',
-    f'copy {input} .ffmpeg-tmp/input.{ext}'.replace('/', '\\'),
+    f'copy "{input}" .ffmpeg-tmp/input.{ext}'.replace('/', '\\'),
     f'{ffmpeg_path} -i .ffmpeg-tmp/input.{ext} -af silenceremove=stop_periods=-1:stop_threshold=0.0005:start_mode=all .ffmpeg-tmp/tmp.wav -y', # Remove Slience
     f'{ffmpeg_path} -i .ffmpeg-tmp/tmp.wav -af apad=pad_dur=0.4 .ffmpeg-tmp/tmp2.wav -y', # Add 0.4s slience at the end
     f'{ffmpeg_path} -i .ffmpeg-tmp/tmp2.wav -af adelay=200:all=true .ffmpeg-tmp/tmp3.wav -y', # Add 0.2s slience at the beginning
-    f'{ffmpeg_path} -i .ffmpeg-tmp/tmp3.wav -filter:a volume=2.5 {outH} -y', # Volume 2.5 times up
-    f'copy {outH} {outS}'.replace('/', '\\'),
+    f'{ffmpeg_path} -i .ffmpeg-tmp/tmp3.wav -filter:a volume=2.5 "{outH}" -y', # Volume 2.5 times up
+    f'copy "{outH}" "{outS}"'.replace('/', '\\'),
     'rmdir /s /q .ffmpeg-tmp',
   ]
 
